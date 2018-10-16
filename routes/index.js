cbffe6526d28d613a015516b7624371595d92e0d
Jee Hoon <yamoo9@naver.com>
2018-10-16 15:02:42 +0900

    routes/index.js 코드 수정

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,8 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   // Pug 페이지 렌더링
   res.render('main', {
-    title: 'y9Bird',
-    user: null,
-    twits: [],
+    title: '메인 페이지 타이틀',
+    // 로그인 오류 발생 시 1회 플래시 처리
     loginError: req.flash('loginError'),
   });
 });
